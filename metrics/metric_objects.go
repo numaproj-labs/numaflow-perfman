ffc6a2d9d5693cfa512272fa0f8f0d44fcb2f972
a3hadi <a3hadi@protonmail.com>
2024-06-21 23:09:45 -0400

    fix spelling mistake
    Signed-off-by: a3hadi <a3hadi@protonmail.com>

--- metrics/metric_objects.go
+++ metrics/metric_objects.go
@@ -33,21 +33,21 @@ var InboundMessages = MetricObject{
 // Latency metrics
 var ForwarderProcessingTimeP90 = MetricObject{
 	Query:    fmt.Sprintf(`histogram_quantile(0.9, rate(forwarder_forward_chunk_processing_time_bucket{pipeline="perfman-base-pipeline"}[%dm%ds])) / 1000000`, minutes, seconds),
-	Filename: "forwarder-e2e-batch-proccessing-time-p90",
+	Filename: "forwarder-e2e-batch-processing-time-p90",
 	XAxis:    "Unix Timestamp",
 	YAxis:    "Seconds",
 }
 
 var ForwarderProcessingTimeP95 = MetricObject{
 	Query:    fmt.Sprintf(`histogram_quantile(0.95, rate(forwarder_forward_chunk_processing_time_bucket{pipeline="perfman-base-pipeline"}[%dm%ds])) / 1000000`, minutes, seconds),
-	Filename: "forwarder-e2e-batch-proccessing-time-p95",
+	Filename: "forwarder-e2e-batch-processing-time-p95",
 	XAxis:    "Unix Timestamp",
 	YAxis:    "Seconds",
 }
 
 var ForwarderProcessingTimeP99 = MetricObject{
 	Query:    fmt.Sprintf(`histogram_quantile(0.99, rate(forwarder_forward_chunk_processing_time_bucket{pipeline="perfman-base-pipeline"}[%dm%ds])) / 1000000`, minutes, seconds),
-	Filename: "forwarder-e2e-batch-proccessing-time-p99",
+	Filename: "forwarder-e2e-batch-processing-time-p99",
 	XAxis:    "Unix Timestamp",
 	YAxis:    "Seconds",
 }
